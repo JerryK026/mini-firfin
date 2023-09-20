eec23ed5db034056ce5912254274e08f952137e3
JerryK026 <kskyung0624@gmail.com>
2023-09-20 14:47:26 +0900

    first commit

---
+++ src/main/java/com/soko/minifirfin/MiniFirfinApplication.java
@@ -0,0 +1,13 @@
+package com.soko.minifirfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFirfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniFirfinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soko/minifirfin/MiniFirfinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soko.minifirfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFirfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
