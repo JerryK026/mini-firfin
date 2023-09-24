1923362bc03eef633a3afb483932f124e8c36c0a
JerryK026 <kskyung0624@gmail.com>
2023-09-25 01:44:45 +0900

    feat : transfer api를 구현한다

--- src/main/java/com/soko/minifirfin/ui/response/RechargeResponse.java
+++ src/main/java/com/soko/minifirfin/ui/response/RechargeResponse.java
@@ -5,12 +5,11 @@ import com.soko.minifirfin.common.Status;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record TransferResponse(
-        String senderName,
-        String receiverName,
-        Long transferHistoryId,
-        BigDecimal transferredMoneyAmount,
-        BigDecimal senderRaminMoneyAmount,
+public record RechargeResponse(
+        String memberName,
+        Long rechargeHistoryId,
+        BigDecimal currentMoneyAmount,
+        BigDecimal rechargedMoneyAmount,
         LocalDateTime timeStamp,
         Status status
 ) { }
